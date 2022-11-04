dc7e7684d034a497710ff8c7aed39b6e857bf658
Josh Derocher-Vlk <josh.derocher-vlk@simplisafe.com>
2022-11-04 10:09:33 -0400

    docs: mark index as internal

--- docs/modules/index.ts.md
+++ docs/modules/index.ts.md
@@ -6,16 +6,6 @@ parent: Modules
 
 ## index overview
 
-Overview
-
-`@jvlk/fp-ts-remote-data` provides a `RemoteData` type that can be used to represent data that is
-in one of 4 possible states:
-
-- loading
-- error
-- empty
-- success
-
 Added in v1.0.0
 
 ---
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 /**
- * Overview
- * 
- * `@jvlk/fp-ts-remote-data` provides a `RemoteData` type that can be used to represent data that is
- * in one of 4 possible states:
- * 
- * - loading
- * - error
- * - empty
- * - success
- * 
- * @category overview
+ * @internal
  * @since 1.0.0
  */
 
